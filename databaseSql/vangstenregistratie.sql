9fc5aa6d85593dc755483abb01ab2e04d8230fcd
Pim Kusters <p.kusters@unilogic.nl>
2015-03-18 12:11:53 +0100

    nieuwe dbsql

--- databaseSql/vangstenregistratie.sql
+++ databaseSql/vangstenregistratie.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 18, 2015 at 01:59 PM
+-- Generation Time: Mar 18, 2015 at 11:23 AM
 -- Server version: 5.5.41-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
+-- PHP Version: 5.5.9-1ubuntu4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,15 +32,7 @@ CREATE TABLE IF NOT EXISTS `catch` (
   `refto_size_id` int(11) NOT NULL,
   `refto_species_id` int(11) NOT NULL,
   `amount` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `catch`
---
-
-INSERT INTO `catch` (`id`, `refto_fishsession_id`, `refto_size_id`, `refto_species_id`, `amount`) VALUES
-(1, 1, 5, 5, 0),
-(2, 1, 1, 17, 0);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -86,14 +78,7 @@ CREATE TABLE IF NOT EXISTS `fishsession` (
   `start` int(11) NOT NULL,
   `stop` int(11) NOT NULL,
   `date` datetime DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `fishsession`
---
-
-INSERT INTO `fishsession` (`id`, `refto_user_id`, `refto_community_id`, `start`, `stop`, `date`) VALUES
-(1, 2, 7, 7, 17, '2015-04-01 00:00:00');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -104,7 +89,7 @@ INSERT INTO `fishsession` (`id`, `refto_user_id`, `refto_community_id`, `start`,
 CREATE TABLE IF NOT EXISTS `size` (
   `id` int(11) NOT NULL,
   `size` varchar(25) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `size`
@@ -114,22 +99,24 @@ INSERT INTO `size` (`id`, `size`) VALUES
 (1, '0 - 5'),
 (2, '5 - 10'),
 (3, '10 - 15'),
-(4, '15 - 25'),
-(5, '25 - 30'),
-(6, '30 - 35'),
-(7, '35 - 40'),
-(8, '40 - 45'),
-(9, '45 - 50'),
-(10, '50 - 55'),
-(11, '55 - 60'),
-(12, '60 - 65'),
-(13, '65 - 70'),
-(14, '70 - 75'),
-(15, '75 - 80'),
-(16, '85 - 90'),
-(17, '90 - 95'),
-(18, '95 - 100'),
-(19, '100+');
+(4, '15 - 20'),
+(5, '15 - 25'),
+(6, '25 - 30'),
+(7, '30 - 35'),
+(8, '35 - 40'),
+(9, '40 - 45'),
+(10, '45 - 50'),
+(11, '50 - 55'),
+(12, '55 - 60'),
+(13, '60 - 65'),
+(14, '65 - 70'),
+(15, '70 - 75'),
+(16, '75 - 80'),
+(17, '80 - 85'),
+(18, '85 - 90'),
+(19, '90 - 95'),
+(20, '95 - 100'),
+(21, '100+');
 
 -- --------------------------------------------------------
 
@@ -191,22 +178,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `firstname` varchar(25) NOT NULL,
   `lastname` varchar(25) NOT NULL,
   `password` varchar(125) NOT NULL,
-  `vispasnr` int(11) DEFAULT NULL,
+  `vispasnr` varchar(25) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
   `refto_community_id` tinyint(4) NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '0',
   `isadmin` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `firstname`, `lastname`, `password`, `vispasnr`, `email`, `refto_community_id`, `active`, `isadmin`) VALUES
-(1, 'pim', 'Pim', 'Kusters', 'test', NULL, 'pim.kusters@gmail.com', 0, 0, 0),
-(2, 'aaa', 'aaa', 'aaa', '$1$3r11z.OE$5Fql.l0hGPu99c60fH03b/', 0, '1', 0, 1, 0),
-(3, 'tests', 'test', '12345', '$2y$10$SdPJlZZSelalKI7U56krFOLTjhFqXX4QiPTE2yfgZ5dohJeDGFAQC', 0, '1', 0, 0, 0),
-(4, 'matt', 'matt', 'worley', '$2y$10$WSUSdi6ycW2Pm3Eiqj5by.76WYkqwT0uyzbFZbd/y2RAZcnIk8ZQC', 12345, '1', 0, 0, 0);
+(1, 'p.kusters', 'pim', 'kusters', '%$UBdpmHa.zKw', '1234567890', 'pim.kusters@gmail.com', 7, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -256,7 +240,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `catch`
 --
 ALTER TABLE `catch`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `community`
 --
@@ -266,12 +250,12 @@ ALTER TABLE `community`
 -- AUTO_INCREMENT for table `fishsession`
 --
 ALTER TABLE `fishsession`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `size`
 --
 ALTER TABLE `size`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT for table `species`
 --
@@ -281,7 +265,7 @@ ALTER TABLE `species`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
