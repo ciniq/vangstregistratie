27c17f3190d0c765db73c45f8a12667eea0e22ce
Pim Kusters <p.kusters@unilogic.nl>
2015-03-18 16:41:58 +0100

    kleine meuk

--- application/models/overview_model.php
+++ application/models/overview_model.php
@@ -38,7 +38,7 @@ class Overview_model extends CI_Model
             foreach ($data as $d)
             {
                 $date = date('d-m-Y', strtotime($d->date));
-                $key = $d->community.'        ('.sprintf('%02d',$d->start).':00 tm '.sprintf('%02d',$d->stop).':00)';
+                $key = $d->community.'        ('.sprintf('%02d',$d->start).'-'.sprintf('%02d',$d->stop).'u)';
                 if(!isset($retval[$date][$key]))
                 {
                     $retval[$date][$key] = array();
